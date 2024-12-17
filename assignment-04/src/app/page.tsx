902b56ed95fee32ad3456a8b906e2388ad115d2a
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-12-17 19:33:00 +0500

    Update page.tsx

--- assignment-04/src/app/page.tsx
+++ assignment-04/src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-5 gap-2 h-screen p-20">
+    <div className="grid grid-cols-5 gap-2 h-screen md:px-20 md:py-10 py-20 px-3">
       <div className="bg-[#FFD900] text-black row-span-5 col-span-1 text-[20px] font-bold flex justify-center items-center">Nav</div>
       <div className="bg-[#FFD900] text-black col-span-4 text-[20px] font-bold flex justify-center items-center">Header</div>
       <div className="bg-[#FFD900] text-black col-span-3 text-[20px] font-bold flex justify-center items-center row-span-3">Artical</div>
