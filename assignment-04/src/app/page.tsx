0547050479168970c25bf417c2aeb1feec96c0bf
Shafique Ur Rehman <kzshafique77@gmail.com>
2024-12-17 19:27:36 +0500

    Assignment 4

---
+++ assignment-04/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-5 gap-2 h-screen p-20">
+      <div className="bg-[#FFD900] text-black row-span-5 col-span-1 text-[20px] font-bold flex justify-center items-center">Nav</div>
+      <div className="bg-[#FFD900] text-black col-span-4 text-[20px] font-bold flex justify-center items-center">Header</div>
+      <div className="bg-[#FFD900] text-black col-span-3 text-[20px] font-bold flex justify-center items-center row-span-3">Artical</div>
+      <div className="bg-[#FFD900] text-black row-span-4 text-[20px] font-bold flex justify-center items-center">Ads</div>
+      <div className="bg-[#FFD900] text-black col-span-3 text-[20px] font-bold flex justify-center items-center">Footer</div>
+    </div> 
+  );
+}
